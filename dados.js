c7f00023101ad4765126d4d84d2b8cee6d1cc476
AlanLoureiro <161273701+AlanLoureiro@users.noreply.github.com>
2024-09-09 18:36:13 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+        titulo: "Lewis Hamilton", 
+        descricao: "Lewis Hamilton é um dos maiores pilotos de Fórmula 1 de todos os tempos. O britânico, com sua habilidade excepcional ao volante e sua personalidade marcante, conquistou sete títulos mundiais, igualando o recorde de Michael Schumacher. Hamilton é conhecido por sua velocidade, ultrapassagens arriscadas e sua luta por igualdade racial dentro e fora das pistas. Sua trajetória na Fórmula 1 é marcada por diversas conquistas históricas e por sua influência como um dos atletas mais famosos do mundo.",
+        link: "https://pt.wikipedia.org/wiki/Lewis_Hamilton",
+        tags:"inglês ingles massa"
+    }, 
+    {
+        titulo: "Michael Schumacher",
+        descricao: "Michael Schumacher é considerado um dos maiores pilotos de Fórmula 1 de todos os tempos. O alemão dominou a categoria por anos, conquistando sete títulos mundiais e estabelecendo diversos recordes. Sua rivalidade com Ayrton Senna marcou uma época e sua precisão ao volante o tornou uma lenda do esporte.",
+        link: "https://pt.wikipedia.org/wiki/Michael_Schumacher",
+        tags:"alemão alemao rubinho"
+    },
+    {
+        titulo: "Ayrton Senna",
+        descricao: "Ayrton Senna é considerado um dos maiores ídolos do esporte brasileiro e um dos pilotos de Fórmula 1 mais talentosos de todos os tempos. O brasileiro encantou o mundo com seu estilo de pilotagem agressivo e sua paixão pela corrida. Sua morte precoce em 1994 deixou um legado inesquecível no automobilismo.",
+        link: "https://pt.wikipedia.org/wiki/Ayrton_Senna",
+        tags:"monâco monaco rei rei de monaco"
+    }
+];
+
